6e70c4d1196703a276acc42c04926ab01f1d5f59
ynakayu <9111754+ynakayu@users.noreply.github.com>
2023-03-30 11:10:01 +0900

    05-03-default-view

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <slot />
+    <footer>&copy; proglus.jp</footer>
+  </div>
+</template>
+<style>
+.container {
+  color: red;
+}
+
+footer {
+  background-color: orange;
+  color: white;
+  text-align: center;
+}
+</style>
